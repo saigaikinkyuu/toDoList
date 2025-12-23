930aee85880d3cb2a4178b2d7fba6a705dd92cf1
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2025-12-23 21:40:24 +0900

    fix: reactのimportの設定

--- js/main.js
+++ js/main.js
@@ -1,35 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
 const TODOLIST = (props) => {
-    return (<div>
-        <h1>TO DO LIST FOR {props.name}</h1>
-        </div>)
+    return (
+        <div>
+            <h1>TO DO LIST FOR {props.name}</h1>
+        </div>
+    )
 }
 
 const CALENDAR = () => {
-    return (<div>
-        <h2>CALENDAR VIEW</h2>
-        </div>)
+    return (
+        <div>
+            <h2>CALENDAR VIEW</h2>
+        </div>
+    )
 }
 
 const BOTTOM_MENUE = () => {
-    return (<div>
-        <button>TODAY</button>
-        <button>TOMORROW</button>
-        <button>CALENDAR</button>
-        </div>)
+    return (
+        <div>
+            <button>TODAY</button>
+            <button>TOMORROW</button>
+            <button>CALENDAR</button>
+        </div>
+    )
 }
 
 const App = () => {
     const [page, setPage] = React.useState("today");
-    if(page === "today" || page === "tomorrow"){
-        return (<div>
-            <TODOLIST name={page}></TODOLIST>
-            <BOTTOM_MENUE></BOTTOM_MENUE>
-            </div>)
-    }else if(page === "calendar"){
-        return (<div>
-            <CALENDAR></CALENDAR>
-            <BOTTOM_MENUE></BOTTOM_MENUE>
-            </div>)
+    if (page === "today" || page === "tomorrow") {
+        return (
+            <div>
+                <TODOLIST name={page}></TODOLIST>
+                <BOTTOM_MENUE></BOTTOM_MENUE>
+            </div>
+        )
+    } else if (page === "calendar") {
+        return (
+            <div>
+                <CALENDAR></CALENDAR>
+                <BOTTOM_MENUE></BOTTOM_MENUE>
+            </div>
+        )
     }
 }
 
